791056ac13729d170da0abb057071d148bdcc754
NoahHig <81652666+NoahHig@users.noreply.github.com>
2024-05-07 12:35:18 -0500

    Add files via upload

---
+++ Godot/HUD.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+class_name HUD
+
+@onready var health_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Health
+@onready var health_gauge = $Control/MarginContainer/VBoxContainer/HBoxContainer/HealthGauge
+@onready var menu = $CenterContainer/Menu
+
+var health = 10:
+	set(new_health):
+		health = new_health
+		_update_health_label()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_update_health_label()
+
+func _update_health_label():
+	health_label.text = str(health)
+	health_gauge.value = health
+
+func _on_damaged(enemy) -> void:
+	if enemy:
+		health -= enemy.get_damage()
+
+
+func _on_button_pressed():
+	get_tree().paused=true
+	menu.show()
